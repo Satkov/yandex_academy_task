d9a7a22ceee0371201ba49359d150bc6faf9cea4
satkov <g.satkov@mail.ru>
2022-06-04 12:11:32 +0300

    Роутинг для модели Product

---
+++ config/products/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from .views import ProductViewSet
+
+router = DefaultRouter()
+router.register(r'boards', ProductViewSet)
+
+
+urlpatterns = format_suffix_patterns([
+    path('nodes/<uuid:pk>/', ProductViewSet.as_view({'get': 'retrieve'}), name='product-retrieve')
+])
