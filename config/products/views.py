1a6429ee02a2fe44508a2b05d4ede4927246bc0e
satkov <g.satkov@mail.ru>
2022-06-04 12:12:02 +0300

    Modelviewset для модели Product

--- config/products/views.py
+++ config/products/views.py
@@ -1,1 +1,12 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from .serializers import ProductSerializer
+from .models import Product
+
+
+class ProductViewSet(mixins.RetrieveModelMixin,
+                     mixins.CreateModelMixin,
+                     mixins.DestroyModelMixin,
+                     GenericViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
