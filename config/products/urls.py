2855133b35e1010755aa78dfcf7e57ca0b8c339e
satkov <g.satkov@mail.ru>
2022-06-06 12:56:54 +0300

    fix urls, delete trailing slash

--- config/products/urls.py
+++ config/products/urls.py
@@ -8,7 +8,7 @@ from .views import ProductViewSet, ProductHistoryViewSet
 # router.register('node/<uuid:pk>/statistic'ProductHistoryViewSet)
 
 urlpatterns = format_suffix_patterns([
-    path('delete/<uuid:pk>/', ProductViewSet.as_view({'delete': 'destroy'}), name='product-destroy'),
-    path('nodes/<uuid:pk>/', ProductViewSet.as_view({'get': 'retrieve'}), name='product-retrieve'),
-    path('imports/', ProductViewSet.as_view({'post': 'create'}), name='product-create'),
+    path('delete/<uuid:pk>', ProductViewSet.as_view({'delete': 'destroy'}), name='product-destroy'),
+    path('nodes/<uuid:pk>', ProductViewSet.as_view({'get': 'retrieve'}), name='product-retrieve'),
+    path('imports', ProductViewSet.as_view({'post': 'create'}), name='product-create'),
 ])
